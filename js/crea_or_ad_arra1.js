657f916284b98e44241e7ff9c6b360a812684ade
igo <igoranatol870@gmail.com>
2022-06-08 17:19:25 +0300

    sdfsdf

--- js/crea_or_ad_arra1.js
+++ js/crea_or_ad_arra1.js
@@ -14,11 +14,87 @@ function creat_or_sol () {
                         var sig_i1_ps1_nm1;
                         var sig_i1_ps1_sr1;
                         var sig_i1_ps1_ag1;
+                        sig_i1_ps1_nm1 = prompt("Введите своё им");
+                        if(sig_i1_ps1_nm1 != sig_i1.sig_i1_us1.sig_i1_us1_nm1 && sig_i1_ps1_nm1 != sig_i1.sig_i1_us1.sig_i1_us1_sr1) throw new Error("У вас ошибка, поел не может быть пуст");
+                        else if(sig_i1_ps1_nm1 == sig_i1.sig_i1_us1.sig_i1_us1_nm1 && sig_i1_ps1_nm1 != sig_i1.sig_i1_us1.sig_i1_us1_sr1) {
+                            sig_i1_ps1_sr1 = prompt("Введите свою фамил");
+                            if(sig_i1_ps1_sr1 != sig_i1.sig_i1_us1.sig_i1_us1_sr1 && sig_i1_ps1_sr1 != sig_i1.sig_i1_us1.sig_i1_us1_ag1) throw new Error("У вас ошибка, поел не может быть пуст");
+                            else if(sig_i1_ps1_sr1 == sig_i1.sig_i1_us1.sig_i1_us1_sr1 && sig_i1_ps1_sr1 != sig_i1.sig_i1_us1.sig_i1_us1_ag1) {
+                                sig_i1_ps1_ag1 = prompt("Введите свой возра");
+                                if(sig_i1_ps1_ag1 != sig_i1.sig_i1_us1.sig_i1_us1_ag1 && sig_i1_ps1_ag1 != sig_i1.sig_i1_us1.sig_i1_us1_sr1) throw new Error("У вас ошибка, поел не может быть пуст");
+                                else if(sig_i1_ps1_ag1 == sig_i1.sig_i1_us1.sig_i1_us1_ag1 && sig_i1_ps1_ag1 != sig_i1.sig_i1_us1.sig_i1_us1_sr1) {
+                                    if(confirm("Ваши данные сохране")) {
+                                        if(confirm("Вы вошли в свой i")) {
+                                            if(confirm("Можете посмотреть свой бала")) {
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             } else {
                 if(confirm("Объе")) {
                     
+                }
+            }
+        }
+    }
+    catch(e) {
+        if(confirm("У вас нету i")) {
+            if(confirm("Можем его сдела")) {
+                if(confirm("За регестрацию id, снимется пла")) {
+                    if(confirm("Введите своё им")) {
+                        var us_i1_nm1;
+                        var us_i1_sr1;
+                        var us_i1_ag1;
+                        us_i1_nm1 = prompt("Введите своё им");
+                        if(us_i1_nm1 != sig_i1.sig_i1_us1.sig_i1_us1_nm1 && us_i1_nm1 != sig_i1.sig_i1_us1.sig_i1_us1_sr1) {
+                            Object.defineProperty(sig_i1, "sig_i1_nm1", {value:us_i1_nm1, writable:true, enumerable:true, configurable:true});
+                            if(sig_i1.hasOwnProperty("sig_i1_nm1") == true && sig_i1.sig_i1_nm1 != sig_i1.sig_i1_us1.sig_i1_us1_nm1) {
+                                us_i1_sr1 = prompt("Введите свою фамил");
+                                if(us_i1_sr1 != sig_i1.sig_i1_us1.sig_i1_us1_sr1 && us_i1_sr1 != sig_i1.sig_i1_us1.sig_i1_us1_ag1) {
+                                    Object.defineProperty(sig_i1, "sig_i1_sr1", {value:us_i1_sr1, writable:true, enumerable:true, configurable:true});
+                                    if(sig_i1.hasOwnProperty("sig_i1_sr1") == true && sig_i1.sig_i1_sr1 != sig_i1.sig_i1_us1.sig_i1_us1_sr1) {
+                                        us_i1_ag1 = prompt("Введите свой возра");
+                                        if(us_i1_ag1 != sig_i1.sig_i1_us1.sig_i1_us1_ag1 && us_i1_ag1 != sig_i1.sig_i1_us1.sig_i1_us1_nm1) {
+                                            Object.defineProperty(sig_i1, "sig_i1_ag1", {value:us_i1_ag1, writable:true, enumerable:true, configurable:true});
+                                            if(sig_i1.hasOwnProperty("sig_i1_ag1") == true && sig_i1.sig_i1_ag1 != sig_i1.sig_i1_us1.sig_i1_us1_ag1) {
+                                                if(confirm("Ваш id, зарегетриров")) {
+                                                    if(confirm("Можете посмотреть свой бала")) {
+                                                        if(confirm("Но, эта функция платна")) {
+                                                            Object.defineProperty(sig_i1, "sig_i1_ba1", {value:"1799", writable:true, enumerable:true, configurable:true});
+                                                            if(sig_i1.hasOwnProperty("sig_i1_ba1") == true && sig_i1.sig_i1_ba1 != sig_i1.sig_i1_us1.sig_i1_us1_ag1) {
+                                                                if(confirm("Ваш бала" + " " + sig_i1.sig_i1_ba1 + "$")) {
+                                                                    if(confirm("Можете посмотреть акци")) {
+                                                                        if(confirm("Эта функция платна")) {
+                                                                            var desc_sal1 = {
+                                                                                desc_sol1_sal1:"appl",
+                                                                                desc_sol1_sal2:"amazo",
+                                                                                desc_sol1_sal3:"samsu",
+                                                                                desc_sol1_sal4:"micro",
+                                                                                desc_sol1_sal5:"windo",
+                                                                            };
+                                                                            if(desc_sal1.hasOwnProperty("desc_sol1_sal1") == true && desc_sal1.hasOwnProperty("desc_sol1_sal3") == true) {
+                                                                                if(confirm("Можете посмотреть акци")) {
+                                                                                    if(confirm("Это всё тоже плат")) {
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
