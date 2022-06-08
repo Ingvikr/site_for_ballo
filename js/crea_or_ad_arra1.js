69a2da190dca5f5d475e20d0ab76b66f43918c95
igo <igoranatol870@gmail.com>
2022-06-08 16:21:40 +0300

    sdfsdfdfsdfsf

---
+++ js/crea_or_ad_arra1.js
@@ -0,0 +1,26 @@
+function creat_or_sol () {
+    try {
+        if(confirm("Вы можете сделать массив, или объект, а потом зарегестрироваться и покупать и продава")) {
+            if(confirm("Что делаем масс")) {
+                if(confirm("Хорошо, вам придётся сделать i")) {
+                    var sig_i1 = {
+                        sig_i1_us1: {
+                            sig_i1_us1_nm1:"igo",
+                            sig_i1_us1_sr1:"romane",
+                            sig_i1_us1_ag1:"19"
+                        }
+                    };
+                    if(sig_i1.hasOwnProperty("sig_i1_us1.sig_i1_us1_nm1") == true && sig_i1.hasOwnProperty("sig_i1_us1.sig_i1_us1_sr1") == true) {
+                        var sig_i1_ps1_nm1;
+                        var sig_i1_ps1_sr1;
+                        var sig_i1_ps1_ag1;
+                    }
+                }
+            } else {
+                if(confirm("Объе")) {
+                    
+                }
+            }
+        }
+    }
+}
